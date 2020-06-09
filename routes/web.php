b0ba71773000ceae9b619d69d4775c7052ce5b90
couch Janus <couchjanus@gmail.com>
2020-06-09 15:51:51 +0300

    add config

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,44 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/env', function () {
+    $environment = App::environment();
+    if (App::environment('local')) {
+        // The environment is local
+        ddd('The environment is local', $environment);
+    }
+    
+    if (App::environment(['local', 'staging'])) {
+        // The environment is either local OR staging...
+        ddd('The environment is either local OR staging...', $environment);
+    }
+    
+
+});
+
+use Carbon\Carbon;
+
+Route::get('/conf', function () {
+
+    dump(Carbon::SUNDAY);                          // int(0)
+    dump(Carbon::MONDAY);                          // int(1)
+    dump(Carbon::TUESDAY);                         // int(2)
+    dump(Carbon::WEDNESDAY);                       // int(3)
+    dump(Carbon::THURSDAY);                        // int(4)
+    dump(Carbon::FRIDAY);                          // int(5)
+    dump(Carbon::SATURDAY);                        // int(6)
+
+    $config = config('database.default');
+    dd($config);
+    $config =Config::get('app.name');
+    dd($config);
+
+});
+
+
+Route::get('/ddd', function () {
+    $user = new App\User();
+    ddd('dumping this one', $user);
+});
